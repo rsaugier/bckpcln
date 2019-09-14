9a16e079ea14273b543cf91626bc83ec4f2883b7
rsaugier <rodolphe.saugier@gmail.com>
2019-09-14 22:39:53 +0200

    initial stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate clap;
+use clap::{Arg, App};
+
+static PROGRAM_NAME : &str = "bckpcln";
+static PROGRAM_VERSION : &str = "1.0";
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new(PROGRAM_NAME)
+        .version(PROGRAM_VERSION)
+        .author("rodolphe saugier <rodolphe.saugier@gmail.com>")
+        .about("BaCKuP CLeaNer. Removes old backups from a directory containing many.")
+        .arg(Arg::with_name("directory")
+             .short("d")
+             .long("directory")
+             .takes_value(true));
+    let args = app.get_matches();
 }
